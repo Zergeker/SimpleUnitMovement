27d0f9e42c2a326af82ccd111d00d01d8507b747
Zergeker <zergec@yandex.ru>
2019-10-21 02:42:31 +0500

    Upload

---
+++ Assets/Scripts/SelectionStatus.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectionStatus : MonoBehaviour
+{
+    UnitSelect mm;
+    void Start()
+    {
+        mm = GameObject.FindObjectOfType<UnitSelect>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (mm.selectedObject != null)
+        {
+            Renderer rend = mm.selectedObject.GetComponentInChildren<Renderer>();
+            float d = rend.bounds.size.x;
+
+            this.transform.position = mm.selectedObject.transform.position;
+            this.transform.localScale = new Vector3(d, -rend.bounds.size.y, d);
+        }
+    }
+}
---
+++ Assets/Scripts/UnitSelect.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine.AI;
+using UnityEngine;
+
+public class UnitSelect : MonoBehaviour
+{
+    public GameObject selectedObject;
+    public string[] selectables = {"Enemy", "Player"};
+    void Update()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        RaycastHit hitUnit;
+
+        if (Physics.Raycast(ray, out hitUnit))
+        {
+            GameObject hitObject = hitUnit.transform.root.gameObject;
+
+            if (Input.GetMouseButton(0) && IsSelectable(hitObject))
+                SelectObject(hitObject);
+
+            if (Input.GetMouseButton(1) && selectedObject.tag == "Player")
+            {
+                if (hitObject.tag == "Enemy")
+                    Destroy(hitUnit.transform.root.gameObject);
+                else
+                    selectedObject.GetComponent<NavMeshAgent>().destination = hitUnit.point;
+            }
+        }
+    }
+
+    void SelectObject(GameObject obj)
+    {
+        if (selectedObject != null)
+        {
+            if (obj == selectedObject)
+                return;
+            ClearSelection();
+        }
+
+        selectedObject = obj;
+        selectedObject.GetComponent<Renderer>().material.EnableKeyword("_EMISSION");
+    }
+    
+    void ClearSelection()
+    {
+        selectedObject.GetComponent<Renderer>().material.DisableKeyword("_EMISSION");
+        selectedObject = null;
+    }
+
+    bool IsSelectable(GameObject obj)
+    {
+        return Array.Exists(selectables, element => element == obj.tag);
+    }
+}
